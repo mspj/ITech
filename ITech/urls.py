3794ffd4d2f57a8bb6d45ccf8969cf490a87f163
Sinderella <rawsec@gmail.com>
2016-02-08 14:23:02 +0000

    Drawed up skeleton files for views in controller

--- ITech/urls.py
+++ ITech/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ITech.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from bookwormsunite.views import index, readathon_info, user_info, user_summary, login, register, search
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'ITech.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^$', index),
+                       url(r'^readathons/(?P<readathon_name_slug>[\w\-]+)/$', readathon_info),
+                       url(r'^user/(?P<uid>[0-9]+)/$', user_info),
+                       url(r'^user/(?P<uid>[0-9]+)/summary/$', user_summary),
+                       url(r'^login/$', login),
+                       url(r'^register/$', register),
+                       url(r'^search/$', search),
+                       url(r'^admin/', include(admin.site.urls)),
+
+                       )
--- bookwormsunite/views.py
+++ bookwormsunite/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    html = "<html><body>index</body></html>"
+    return HttpResponse(html)
+
+
+def readathon_info(request, readathon_name_slug):
+    html = "<html><body>readathon_info: {0}</body></html>".format(readathon_name_slug)
+    return HttpResponse(html)
+
+
+def user_info(request, uid):
+    html = "<html><body>user_info: {0}</body></html>".format(uid)
+    return HttpResponse(html)
+
+
+def user_summary(request, uid):
+    html = "<html><body>user_summary: {0}</body></html>".format(uid)
+    return HttpResponse(html)
+
+
+def login(request):
+    if request.method != "POST":
+        return HttpResponse('Method not allowed', status=405)
+    html = "<html><body>login</body></html>"
+    return HttpResponse(html)
+
+
+def register(request):
+    if request.method != "POST":
+        return HttpResponse('Method not allowed', status=405)
+    html = "<html><body>register</body></html>"
+    return HttpResponse(html)
+
+
+def search(request):
+    if request.method != "POST":
+        return HttpResponse('Method not allowed', status=405)
+    html = "<html><body>search</body></html>"
+    return HttpResponse(html)

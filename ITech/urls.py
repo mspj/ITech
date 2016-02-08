5cea198be86cfa7479a702656e79b1244d91c574
Sinderella <rawsec@gmail.com>
2016-02-08 15:16:31 +0000

    Renamed url, removed s from readathons

--- ITech/urls.py
+++ ITech/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^$', index),
-                       url(r'^readathons/(?P<readathon_name_slug>[\w\-]+)/$', readathon_info),
+                       url(r'^readathon/(?P<readathon_name_slug>[\w\-]+)/$', readathon_info),
                        url(r'^user/(?P<uid>[0-9]+)/$', user_info),
                        url(r'^user/(?P<uid>[0-9]+)/summary/$', user_summary),
                        url(r'^login/$', login),
